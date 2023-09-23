034dcdff9486063d852761249d9c43bb281047e9
Gustavo404 <60305898+Gustavo404@users.noreply.github.com>
2023-09-22 21:46:09 -0300

    Update obsidian.sh
    adição de args na linha de comando,
    melhoria dos dialogos,
    melhor detecção de erros.

--- obsidian.sh
+++ obsidian.sh
@@ -23,32 +23,73 @@ function color_message() {
   esac
 }
 
+# ...
+
 # Diálogo de boas-vindas
-clear
-color_message "green" "Bem-vindo ao Obsidian"
-echo -e "Este script irá filtrar as linhas com valores de dBm inferiores ao limite especificado."
-echo -e "exemplos de valores:    maximo: -26    minimo: -39"
-# Solicita o limite maximo de valor em dBm ao usuário
-echo
-color_message "yellow" "\n[...] Por favor, insira o limite maximo:"
-read teto_dBm
+# Verifica se foram fornecidos argumentos de linha de comando
+if [ $# -eq 0 ]; then
+  # Diálogo de boas-vindas
+  clear
+  color_message "green" "Bem-vindo ao Obsidian"
+  echo -e "Este script irá filtrar as linhas com valores de dBm inferiores ao limite especificado."
+  echo -e "exemplos de valores:    maximo: -26    minimo: -39"
 
-# Solicita o limite minimo de valor em dBm ao usuário
-echo
-color_message "yellow" "\n[...] Por favor, insira o limite minimo inferior a $teto_dBm:"
-read piso_dBm
+  # Solicita o limite maximo de valor em dBm ao usuário
+  echo
+  color_message "yellow" "\n[...] Por favor, insira o limite maximo:"
+  read teto_dBm
 
-# Solicita o nome do arquivo de entrada
-echo
-color_message "yellow" "[...] Agora, insira o nome do arquivo de entrada:"
-read input
+  # Solicita o limite minimo de valor em dBm ao usuário
+  echo
+  color_message "yellow" "\n[...] Por favor, insira o limite minimo inferior a $teto_dBm:"
+  read piso_dBm
 
-# Solicita o nome do arquivo de saída
-echo
-color_message "yellow" "[...] Insira o nome do arquivo de saída:"
-read output
+  # Solicita o nome do arquivo de entrada
+  echo
+  color_message "yellow" "[...] Agora, insira o nome do arquivo de entrada:"
+  read input
 
-color_message "blue" "[ ! ] Efetuando alterações!"
+  # Solicita o nome do arquivo de saída
+  echo
+  color_message "yellow" "[...] Insira o nome do arquivo de saída:"
+  read output
+else
+  # Processa as opções da linha de comando
+  while getopts "t:p:i:o:" opt; do
+    case $opt in
+      t)
+        teto_dBm="$OPTARG"
+        ;;
+      p)
+        piso_dBm="$OPTARG"
+        ;;
+      i)
+        input="$OPTARG"
+        ;;
+      o)
+        output="$OPTARG"
+        ;;
+      \?)
+        echo "Opção inválida: -$OPTARG" >&2
+        exit 1
+        ;;
+      :)
+        echo "A opção -$OPTARG requer um argumento." >&2
+        exit 1
+        ;;
+    esac
+  done
+
+  # Verifica se todas as opções obrigatórias foram fornecidas
+  if [[ -z "$teto_dBm" || -z "$piso_dBm" || -z "$input" || -z "$output" ]]; then
+    color_message "red" "Erro: Você deve fornecer todas as opções -t, -p, -i e -o."
+    exit 1
+  fi
+fi
+
+# ...
+
+color_message "blue" "[!] Efetuando alterações!"
 # Aplicando o filtro com AWK usando o limite especificado
 awk -v teto="$teto_dBm" -F'\t' '$2 >= teto' "$input" > .tmp
 awk -v piso="$piso_dBm" -F'\t' '$2 <= piso' .tmp > "$output"
@@ -61,22 +102,31 @@ sleep 0.5
 if [ -e "$output" ]; then
   color_message "green" "[!] O arquivo $output criado com sucesso!"
   rm .tmp
-else
+  else
   color_message "red" "[!] Houve um erro ao criar o arquivo $output, verifique obsidian/.tmp"
 fi
 
 # Extrair clientes com sinal igual a -40 dBm
-read -p "[?] Deseja extrair os clientes com sinal igual a -40 dBm? (Y/n)" response
+read -p "[?] Deseja copiar os clientes com sinal igual a -40 dBm de $input para 40dBm_$input.txt? (Y/n)" response
 if [ "$response" = "n" ] || [ "$response" = "N" ]; then
-  color_message "yellow" "[ . ] Processo finalizado."
-  sleep 1
-  exit
-else
-  color_message "blue" "[...] Prosseguindo..."
-  awk -F'\t' '$2 == -40' "$input" > 40dBm.txt
+  color_message "yellow" "[!] Processo finalizado."
+  sleep 0.5
+  else
+  color_message "blue" "[.] Prosseguindo..."
+  awk -F'\t' '$2 == -40' "$input" > 40dBm_$input.txt
 fi
 
 # Mensagem de conclusão
-color_message "green" "\nFiltro concluído com sucesso!"
-echo -e "As linhas com valores de dBm superiores a $teto_dBm foram removidas."
-echo -e "Os resultados foram salvos em \e[1m$output\e[0m."
+if [[ -e "$output" ]]; then
+  color_message "green" "\n-----------------------------"
+  color_message "green" "Filtro concluído com sucesso!"
+  if [[ -e "40dBm_$input.txt" ]]; then
+    echo -e "\e[92m[!]\e[0m As linhas com sinal igual a \e[94m-40 dBm\e[0m foram salvas em \e[94m40dBm_$input.txt\e[0m"
+  fi
+  echo -e "\e[92m[!]\e[0m As linhas com valores de dBm superiores a \e[94m$teto_dBm\e[0m, e inferiores a \e[94m$piso_dBm\e[0m foram removidas."
+  echo -e "\e[92m[!]\e[0m Os resultados foram salvos em \e[94m$output\e[0m."
+else
+  color_message "red" "\n-----------------------------"
+  color_message "red" "[!] Ocorreu um erro!"
+  exit 
+fi
