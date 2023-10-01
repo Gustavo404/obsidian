ec4ed7846a322330c7dad36796dbc1e62e02acd1
gustavo404 <gustavo404.marcio@gmail.com>
2023-10-01 13:22:37 -0300

    melhoria nas funções e organização do código

--- obsidian.sh
+++ obsidian.sh
@@ -23,50 +23,33 @@ function color_message() {
   esac
 }
 
-# Diálogo de boas-vindas
-# Verifica se foram fornecidos argumentos de linha de comando
-if [ $# -eq 0 ]; then
-  # Diálogo de boas-vindas
-  clear
-  color_message "green" "Bem-vindo ao Obsidian"
-  echo -e "Este script irá filtrar as linhas com valores de dBm inferiores ao limite especificado."
-  echo -e "exemplos de valores:    maximo: -26    minimo: -39"
-
+# Função para solicitar os parâmetros de entrada
+function solicitar_parametros() {
   # Solicita o limite maximo de valor em dBm ao usuário
-  echo
   color_message "yellow" "\n[...] Por favor, insira o limite maximo:"
   read teto_dBm
 
   # Solicita o limite minimo de valor em dBm ao usuário
-  echo
   color_message "yellow" "\n[...] Por favor, insira o limite minimo inferior a $teto_dBm:"
   read piso_dBm
 
   # Solicita o nome do arquivo de entrada
-  echo
   color_message "yellow" "[...] Agora, insira o nome do arquivo de entrada:"
   read input
 
   # Solicita o nome do arquivo de saída
-  echo
   color_message "yellow" "[...] Insira o nome do arquivo de saída:"
   read output
-else
-  # Processa as opções da linha de comando
+}
+
+# Função para processar as opções da linha de comando
+function processar_opcoes() {
   while getopts "t:p:i:o:" opt; do
     case $opt in
-      t)
-        teto_dBm="$OPTARG"
-        ;;
-      p)
-        piso_dBm="$OPTARG"
-        ;;
-      i)
-        input="$OPTARG"
-        ;;
-      o)
-        output="$OPTARG"
-        ;;
+      t) teto_dBm="$OPTARG" ;;
+      p) piso_dBm="$OPTARG" ;;
+      i) input="$OPTARG" ;;
+      o) output="$OPTARG" ;;
       \?)
         color_message "red" "Opção inválida: -$OPTARG"
         exit 1
@@ -83,63 +66,94 @@ else
     color_message "red" "Erro: Você deve fornecer todas as opções -t, -p, -i e -o."
     exit 1
   fi
-fi
+}
 
-# Verifica se o limite máximo é um número válido
-if ! [[ "$teto_dBm" =~ ^-?[0-9]+$ ]]; then
-  color_message "red" "O limite máximo '$teto_dBm' não é um número válido."
-  exit 1
-fi
-
-# Verifica se o limite mínimo é um número válido
-if ! [[ "$piso_dBm" =~ ^-?[0-9]+$ ]]; then
-  color_message "red" "O limite mínimo '$piso_dBm' não é um número válido."
-  exit 1
-fi
-
-# Verifica se o arquivo de entrada existe
-if [ ! -f "$input" ]; then
-  color_message "red" "O arquivo de entrada '$input' não existe."
-  exit 1
-fi
-
-# Aplicando o filtro com AWK usando o limite especificado
-awk -v teto="$teto_dBm" -F'\t' '$3 >= teto' "$input" > .tmp
-awk -v piso="$piso_dBm" -F'\t' '$3 <= piso' .tmp > "$output"
-
-# Aplicando o filtro com SED para remover linhas vazias
-sed -i '/^$/d' "$output"
-sleep 0.5
-
-# Verifica se o arquivo foi criado com sucesso
-if [ -e "$output" ]; then
-  color_message "green" "[!] O arquivo $output criado com sucesso!"
-  rm .tmp
-else
-  color_message "red" "[!] Houve um erro ao criar o arquivo $output, verifique obsidian/.tmp"
-fi
-
-# Extrair clientes com sinal igual a -40 dBm
-read -p "[?] Deseja copiar os clientes com sinal igual a -40 e 0.00 dBm, e com erro de $input para 40dBm_$input.txt? (Y/n)" response
-if [ "$response" = "n" ] || [ "$response" = "N" ]; then
-  color_message "yellow" "[!] Processo finalizado."
-  sleep 0.5
-else
-  color_message "blue" "[.] Prosseguindo..."
-  grep '\(-40\|RECV POWER\|(Dbm)\|\[ ERR\|onu\)' "$input" > 40dbm_$input
-fi
-
-# Mensagem de conclusão
-if [[ -e "$output" ]]; then
-  color_message "green" "\n-----------------------------"
-  color_message "green" "Filtro concluído com sucesso!"
-  if [[ -e "40dBm_$input.txt" ]]; then
-    echo -e "\e[92m[!]\e[0m As linhas com sinal igual a \e[94m-40 dBm\e[0m foram salvas em \e[94m40dBm_$input.txt\e[0m"
+# Função para verificar se os limites de dBm são números válidos
+function verificar_limites() {
+  if ! [[ "$teto_dBm" =~ ^-?[0-9]+$ ]]; then
+    color_message "red" "O limite máximo '$teto_dBm' não é um número válido."
+    exit 1
   fi
-  echo -e "\e[92m[!]\e[0m As linhas com valores de dBm superiores a \e[94m$teto_dBm\e[0m, e inferiores a \e[94m$piso_dBm\e[0m foram removidas."
-  echo -e "\e[92m[!]\e[0m Os resultados foram salvos em \e[94m$output\e[0m."
+  if ! [[ "$piso_dBm" =~ ^-?[0-9]+$ ]]; then
+    color_message "red" "O limite mínimo '$piso_dBm' não é um número válido."
+    exit 1
+  fi
+}
+
+# Função para verificar se o arquivo de entrada existe
+function verificar_arquivo_entrada() {
+  if [ ! -f "$input" ]; then
+    color_message "red" "O arquivo de entrada '$input' não existe."
+    exit 1
+  fi
+}
+
+# Função para aplicar o filtro com AWK usando o limite especificado
+function aplicar_filtro_awk() {
+  awk -v teto="$teto_dBm" -F'\t' '$3 >= teto' "$input" > .tmp
+  awk -v piso="$piso_dBm" -F'\t' '$3 <= piso' .tmp > "$output"
+}
+
+# Função para aplicar o filtro com SED para remover linhas vazias
+function aplicar_filtro_sed() {
+  sed -i '/^$/d' "$output"
+  sleep 0.5
+}
+
+# Função para verificar se o arquivo foi criado com sucesso
+function verificar_arquivo_saida() {
+  if [ -e "$output" ]; then
+    color_message "green" "[!] O arquivo $output criado com sucesso!"
+    rm .tmp
+  else
+    color_message "red" "[!] Houve um erro ao criar o arquivo $output, verifique obsidian/.tmp"
+  fi
+}
+
+# Função para extrair clientes com sinal igual a -40 dBm
+function extrair_clientes_40dbm() {
+  read -p "[?] Deseja copiar os clientes com sinal igual a -40 e 0.00 dBm, e com erro de $input para 40dBm_$input.txt? (Y/n)" response
+  if [ "$response" = "n" ] || [ "$response" = "N" ]; then
+    color_message "yellow" "[!] Processo finalizado."
+    sleep 0.5
+  else
+    color_message "blue" "[.] Prosseguindo..."
+    grep '\(-40\|RECV POWER\|(Dbm)\|\[ ERR\|onu\)' "$input" > 40dbm_$input
+  fi
+}
+
+# Função para exibir a mensagem de conclusão
+function exibir_mensagem_conclusao() {
+  if [[ -e "$output" ]]; then
+    color_message "green" "\n-----------------------------"
+    color_message "green" "Filtro concluído com sucesso!"
+    if [[ -e "40dBm_$input.txt" ]]; then
+      echo -e "\e[92m[!]\e[0m As linhas com sinal igual a \e[94m-40 dBm\e[0m foram salvas em \e[94m40dBm_$input.txt\e[0m"
+    fi
+    echo -e "\e[92m[!]\e[0m As linhas com valores de dBm superiores a \e[94m$teto_dBm\e[0m, e inferiores a \e[94m$piso_dBm\e[0m foram removidas."
+    echo -e "\e[92m[!]\e[0m Os resultados foram salvos em \e[94m$output\e[0m."
+  else
+    color_message "red" "\n-----------------------------"
+    color_message "red" "[!] Ocorreu um erro!"
+    exit 1
+  fi
+}
+
+# Diálogo de boas-vindas
+if [ $# -eq 0 ]; then
+  clear
+  color_message "green" "Bem-vindo ao Obsidian"
+  echo -e "Este script irá filtrar as linhas com valores de dBm inferiores ao limite especificado."
+  echo -e "exemplos de valores:    maximo: -26    minimo: -39"
+  solicitar_parametros
 else
-  color_message "red" "\n-----------------------------"
-  color_message "red" "[!] Ocorreu um erro!"
-  exit 1
+  processar_opcoes "$@"
 fi
+
+verificar_limites
+verificar_arquivo_entrada
+aplicar_filtro_awk
+aplicar_filtro_sed
+verificar_arquivo_saida
+extrair_clientes_40dbm
+exibir_mensagem_conclusao
